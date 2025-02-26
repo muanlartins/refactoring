cd5183bd050a4348d7e5313c565d1de409f41f5a
muanlartins <muanlartins@gmail.com>
2025-02-26 19:12:47 -0300

    feat: setup statement v1

--- tests/statement.test.js
+++ tests/statement.test.js
@@ -1,14 +1,13 @@
-import { statement } from "../src/v0/statement.js";
+import v0 from "../src/v0/statement.js";
+import v1 from "../src/v1/statement.js";
 import plays from "../plays.json" with { type: "json" };
 import invoices from "../invoices.json" with { type: "json" };
 
 test('Statement result', () => {
-  expect(statement(invoices[0], plays)).toBe(
-`Statement for BigCo
- Hamlet: $650.00 (55 seats)
- As You Like It: $580.00 (35 seats)
- Othello: $500.00 (40 seats)
-Amount owed is $1,730.00
-You earned 47 credits
-`);
+  const invoice = invoices[0];
+
+  const v0Statement = v0(invoice, plays);
+  const v1Statement = v1(invoice, plays);
+
+  expect(v1Statement).toBe(v0Statement);
 });
