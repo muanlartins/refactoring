da9a99a8f0fde0bdedab215f0fce92aadab0dab8
muanlartins <muanlartins@gmail.com>
2025-02-26 21:24:23 -0300

    feat: v1 refactoring

--- src/v1/statement.js
+++ src/v1/statement.js
@@ -1,41 +1,90 @@
 export default function statement(invoice, plays) {
-  let totalAmount = 0; 
-  let volumeCredits = 0; 
-  let result = `Statement for ${invoice.customer}\n`; 
-  const format = new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format; 
-  for (let perf of invoice.performances) {
-    const play = plays[perf.playID]; 
-    let thisAmount = 0; 
-    
-    switch (play.type) { 
-      case "tragedy": 
-        thisAmount = 40000; 
-        if (perf.audience > 30) { 
-          thisAmount += 1000 * (perf.audience - 30); 
-        } 
-        break; 
-      case "comedy": 
-        thisAmount = 30000; 
-        if (perf.audience > 20) { 
-          thisAmount += 10000 + 500 * (perf.audience - 20); 
-        } 
-        thisAmount += 300 * perf.audience; 
-        break; 
-      default: 
-        throw new Error(`unknown type: ${play.type}`); 
-    }
-    
-    // add volume credits 
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    // add extra credit for every ten comedy attendees 
-    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-    
-    // print line for this order 
-    result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`; 
-    totalAmount += thisAmount;
-  } 
+  enrichInvoice(invoice, plays);
   
-  result += `Amount owed is ${format(totalAmount / 100)}\n`; 
-  result += `You earned ${volumeCredits} credits\n`; 
-  return result;
+  return consolidateStatementReport(invoice);
+}
+
+function enrichInvoice(invoice, plays) {
+  invoice.performances.forEach((performance) => enrichPerformance(performance, plays));
+
+  invoice.amount = invoice.performances.reduce((sum, performance) => sum + performance.amount, 0);
+  invoice.credits = invoice.performances.reduce((sum, performance) => sum + performance.credits, 0);
+}
+
+function enrichPerformance(performance, plays) {
+  getPerformancePlay(performance, plays);
+
+  const calculator = getCalculator(performance);
+  calculator.calculateAmount(performance);
+  calculator.calculateCredits(performance);
+}
+
+function getPerformancePlay(performance, plays) {
+  performance.play = plays[performance.playID];
+}
+
+function getCalculator(performance) {
+  switch (performance.play.type) {
+    case "tragedy": 
+      return TragedyCalculator;
+    case "comedy": 
+      return ComedyCalculator;
+    default: 
+      throw new Error(`unknown type: ${performance.play.type}`); 
+  }
+}
+
+class AmountCalculator {
+  constructor() {}
+
+  calculateAmount(performance) {}
+
+  calculateCredits(performance) {}
+}
+
+class ComedyCalculator extends AmountCalculator {
+  static calculateAmount(performance) {
+    performance.amount = 30000; 
+
+    if (performance.audience > 20) { 
+      performance.amount += 10000 + 500 * (performance.audience - 20); 
+    }
+
+    performance.amount += 300 * performance.audience; 
+  }
+
+  static calculateCredits(performance) {
+    performance.credits = Math.max(performance.audience - 30, 0) + Math.floor(performance.audience / 5);
+  }
+}
+
+class TragedyCalculator extends AmountCalculator {
+  static calculateAmount(performance) {
+    performance.amount = 40000; 
+
+    if (performance.audience > 30) { 
+      performance.amount += 1000 * (performance.audience - 30); 
+    } 
+  }
+
+  static calculateCredits(performance) {
+    performance.credits = Math.max(performance.audience - 30, 0);
+  }
+}
+
+function consolidateStatementReport(invoice) {
+  let report = `Statement for ${invoice.customer}\n`; 
+
+  invoice.performances.forEach((performance) => 
+    report += ` ${performance.play.name}: ${convertToUsd(performance.amount / 100)} (${performance.audience} seats)\n`
+  );
+
+  report += `Amount owed is ${convertToUsd(invoice.amount / 100)}\n`;
+  report += `You earned ${invoice.credits} credits\n`; 
+
+  return report;
+}
+
+function convertToUsd(value) {
+  return new Intl.NumberFormat("en-US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format(value);
 }
